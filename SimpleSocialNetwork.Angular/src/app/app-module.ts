0f841c5b9475d8ad371fa39033faea3624ec78d8
nmartinyuk <n.martinyuk@outlook.com>
2025-10-31 23:22:31 +0200

    +

--- SimpleSocialNetwork.Angular/src/app/app-module.ts
+++ SimpleSocialNetwork.Angular/src/app/app-module.ts
@@ -1,31 +1,9 @@
-import { NgModule, provideBrowserGlobalErrorListeners, APP_INITIALIZER } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing-module';
-import { App } from './app';
-import { LoginComponent } from './login/login';
-import { AppConfigService } from './app-config.service';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { apiBaseInterceptor } from './api-base.interceptor';
-
-export function initConfig(cfg: AppConfigService) {
-  return () => cfg.load();  // APP_INITIALIZER ждёт Promise из cfg.load()
-}
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app-component';
 
 @NgModule({
   declarations: [
-    App,
-    LoginComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideHttpClient(withInterceptors([apiBaseInterceptor])),
-    provideBrowserGlobalErrorListeners(),
-    { provide: APP_INITIALIZER, useFactory: initConfig, deps: [AppConfigService], multi: true }
-  ],
-  bootstrap: [App]
+    AppComponent
+  ]
 })
 export class AppModule { }

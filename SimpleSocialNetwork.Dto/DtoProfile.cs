402f0f9f83732498a1b1f6a13bf12e1aad5ad8a6
nmartinyuk <n.martinyuk@outlook.com>
2025-11-27 17:52:15 +0200

    +

--- SimpleSocialNetwork.Dto/DtoProfile.cs
+++ SimpleSocialNetwork.Dto/DtoProfile.cs
@@ -5,17 +5,24 @@ namespace SimpleSocialNetwork.Dto
     public class DtoProfile
     {
         [DataMember]
-        public string email { get; set; }
+        public string? email { get; set; } // Добавили ?
+
         [DataMember]
-        public string name { get; set; }
+        public string? name { get; set; } // Добавили ?
+
         [DataMember]
-        public string password { get; set; }
+        public string? password { get; set; } // Добавили ?
+
+        // ЭТО ПОЛЕ ВЫЗЫВАЛО ОШИБКУ: Angular его не шлет, а оно было обязательным
         [DataMember]
-        public string token { get; set; }
+        public string? token { get; set; } 
+
         [DataMember]
         public bool verified { get; set; }
+
         [DataMember]
         public bool isAdmin { get; set; }
+
         [DataMember]
         public int? messagesLeft { get; set; }
     }

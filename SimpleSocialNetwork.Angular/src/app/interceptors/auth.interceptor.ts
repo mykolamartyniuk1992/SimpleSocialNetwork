d252f79bfcfbc0a860052fc404688cb1ed65dd60
nmartinyuk <n.martinyuk@outlook.com>
2025-11-30 19:03:01 +0200

    fix infinite loop dependency (SettingsService - AuthInterceptor - AuthService - SetingsService etc)

--- SimpleSocialNetwork.Angular/src/app/interceptors/auth.interceptor.ts
+++ SimpleSocialNetwork.Angular/src/app/interceptors/auth.interceptor.ts
@@ -3,9 +3,12 @@ import { inject } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  // Не инжектировать AuthService для запросов к конфигу
+  if (req.url.includes('appconfig.json') || req.url.includes('appconfig.production.json')) {
+    return next(req);
+  }
   const authService = inject(AuthService);
   const token = authService.getToken();
-
   if (token) {
     const cloned = req.clone({
       setHeaders: {
@@ -14,6 +17,5 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
     });
     return next(cloned);
   }
-
   return next(req);
 };

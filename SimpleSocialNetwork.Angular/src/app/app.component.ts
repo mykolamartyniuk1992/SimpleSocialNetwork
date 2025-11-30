5ad06092a488a6558ee98381edb2af9ca9a2ce79
nmartinyuk <n.martinyuk@outlook.com>
2025-11-30 21:00:10 +0200

    hide profile picture, header buttons if not authenticated

--- SimpleSocialNetwork.Angular/src/app/app.component.ts
+++ SimpleSocialNetwork.Angular/src/app/app.component.ts
@@ -1,8 +1,10 @@
+
 import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer.component';
 import { environment } from '../environments/environment';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -11,8 +13,24 @@ import { environment } from '../environments/environment';
   imports: [RouterOutlet, HeaderComponent, FooterComponent]
 })
 export class AppComponent implements OnInit {
+  public showLayout = false;
+
+  constructor(private router: Router, private authService: AuthService) {
+    // this.router.events is an Observable that emits events when navigation happens (route changes)
+    this.router.events.subscribe(() => {
+      this.updateLayout();
+    });
+    this.updateLayout();
+  }
+
   ngOnInit() {
-    // Apply theme based on environment
     document.body.classList.add(`theme-${environment.theme}`);
+    this.updateLayout();
+  }
+
+  private updateLayout() {
+    const path = this.router.url;
+    // Header показывается только если пользователь авторизован и не на /login или /register
+    this.showLayout = this.authService.isAuthenticated() && !['/login', '/register'].includes(path);
   }
 }
